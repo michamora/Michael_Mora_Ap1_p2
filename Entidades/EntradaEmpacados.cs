babe54b62814ef6fde864232fc2bac1546711aee
micha mora <michamora4@gmail.com>
2022-03-16 16:55:04 -0400

    Empacados Entidad

--- Entidades/EntradaEmpacados.cs
+++ Entidades/EntradaEmpacados.cs
@@ -5,24 +5,29 @@ namespace Parcial2.Models
 {
 public class EntradaEmpacados
 {
+        [Key]
+        public int EmpacadosId {get; set;}
+        public int EmpacadosDetalleId {get; set;}
+        
+      
 
-    [Key]
-    public int EmpacadosId { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio. colocar fecha actual.")]
+        public DateTime Fecha {get; set;} = DateTime.Now;
 
-    [Required]
-    [MaxLength(80, ErrorMessage = "El concepto no puede exceder {1} caracteres")]
-    [MinLength(3, ErrorMessage = "El concepto debe tener al menos {1} caracteres")]
-    public string? Concepto { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio. Se debe indicar concepto. ")]
+        [MinLength(3, ErrorMessage = "El concepto debe tener almenos {1} caracteres.")]
+        [MaxLength(35, ErrorMessage = "El concepto no debe pasar de {1} caracteres. ")]
+        public string? Concepto { get; set; }
 
-    [Required]
-    [DataType(DataType.Date)]
-    public DateTime Fecha { get; set; } = DateTime.Now;
+        public int CantidadUtilizada {get; set;}
 
-    [Required]
+        public int CantidadProducida {get; set;}
 
-    public ICollection<PresentacionDetalles> PresentacionDetalles { get; set; }
-    public ICollection<ProductosUtilizados> PUtilizados { get; set; }
+   
 
-    public ProductosDetalle? PProducidos { get; set; }
-}
+        [ForeignKey("EmpacadosId")]
+
+       public ICollection<EmpacadosDetalle> EmpacadosDetalle { get; set; } 
+
+    }
 }
